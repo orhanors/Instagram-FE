67dce944e157a135ee34747e99a4ddc74f5f153e
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2021-03-01 16:04:36 +0100

    adds routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Profile from "./pages/profile/Profile";
+import Home from "./pages/home/Home";
 
 function App(props) {
-	return <div>APP</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/me" component={Profile} exact />
+	  </Switch>
+    </Router>
+  );
 }
 
 export default App;
