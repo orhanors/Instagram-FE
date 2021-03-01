4ef4c530d176c08895200cc108fd1caaddd01ac3
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2021-03-01 23:54:50 +0100

    adds profile posts menu

--- src/components/profile/PostsArea.jsx
+++ src/components/profile/PostsArea.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Profile.scss";
 import { Container, Row, Col } from 'react-bootstrap';
 import {MdGridOn} from 'react-icons/md';
@@ -7,21 +7,24 @@ import {BiBookmark} from 'react-icons/bi';
 import {BiUserPin} from 'react-icons/bi';
 
 const PostsArea = () => {
+
+    const[activeItem, setActiveItem] = useState("POSTS");
+
     return (
         <Container className="container">
             <hr />
             <Row className="items-container">
-            <div className="item">
-                <MdGridOn/> POSTS
+            <div className="active-item" onClick={()=>setActiveItem("POSTS")}>
+                <MdGridOn/> <p>POSTS</p>
             </div>
-            <div className="item">
-                <FiMonitor/>IGTV
+            <div className="item" onClick={()=>setActiveItem("IGTV")}>
+                <FiMonitor/><p>IGTV</p>
             </div>
-            <div className="item">
-                <BiBookmark/> SAVED
+            <div className="item" onClick={()=>setActiveItem("SAVED")}>
+                <BiBookmark/> <p>SAVED</p>
             </div>
-            <div className="item">
-                <BiUserPin/>TAGGED
+            <div className="item" onClick={()=>setActiveItem("TAGGED")}>
+                <BiUserPin/><p>TAGGED</p>
             </div>
             </Row>
             
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import NavBar from "../../components/nav-bar/NavBar";
 import ProfileInfo from "../../components/profile/ProfileInfo";
 import ProfileArea from '../../components/profile/PostsArea';
 
