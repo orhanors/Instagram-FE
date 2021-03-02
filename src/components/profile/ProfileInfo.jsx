9fca47b5a6c267ef5c3458204f683e155c46dfc0
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2021-03-02 16:03:40 +0100

    adds following, message... btns

--- src/components/profile/ProfileInfo.jsx
+++ src/components/profile/ProfileInfo.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "./Profile.scss";
-import {IoIosSettings} from 'react-icons/io'
+import { IoIosSettings } from "react-icons/io";
+import { FaUserCheck } from "react-icons/fa";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { HiDotsHorizontal } from 'react-icons/hi'
 
 const ProfileInfo = () => {
+  let [me, setMe] = useState(false);
+  let [following, setFollowing] = useState(false);
+
   return (
     <Container className="container">
       <Row>
@@ -13,18 +19,51 @@ const ProfileInfo = () => {
         <Col xs={11} md={8} mt-4>
           <Row className="firstRow">
             <p className="username">username</p>
-            <button className="editProfie-btn">
-              <strong>Edit Profile</strong>
-            </button>
-            <IoIosSettings className='settings-btn'/>
-          </Row>
-          <Row className='firstRow'>
-            <p><strong>5</strong> posts</p>
-            <p><strong>110</strong> followers</p>
-            <p><strong>100</strong> following</p>
+            {me && (
+              <>
+                <button className="editProfie-btn">
+                  <strong>Edit Profile</strong>
+                </button>
+                <IoIosSettings className="settings-btn" />
+              </>
+            )}
+            {(following ) ? ( !me &&
+              <div className="following-btns">
+                <button className="message-btn">Message</button>
+                <button className="following">
+                  <FaUserCheck />
+                </button>
+                <button className="sugested">
+                  <IoMdArrowDropdown />
+                </button>
+                <HiDotsHorizontal/>
+              </div>
+            ) : (
+              !me && <div className="follow-btns">
+                <button className="follow-btn">Follow</button>
+                <button className="more">
+                  <IoMdArrowDropdown />
+                </button>
+                <HiDotsHorizontal/>
+              </div>
+            )}
+            
           </Row>
           <Row className="firstRow">
-            <p><strong>name</strong></p>
+            <p>
+              <strong>5</strong> posts
+            </p>
+            <p>
+              <strong>110</strong> followers
+            </p>
+            <p>
+              <strong>100</strong> following
+            </p>
+          </Row>
+          <Row className="firstRow">
+            <p>
+              <strong>name</strong>
+            </p>
           </Row>
         </Col>
       </Row>
